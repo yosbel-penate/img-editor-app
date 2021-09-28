f75a3b56aa3ef3a2107479735622b4bc8b6684f6
yosbel-penate <yosbel.penate@gmail.com>
2021-09-29 02:03:16 +0300

    +++

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import MyEditor from './componets/myeditor/myeditor.js'
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Slider from '@mui/material/Slider';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <MyEditor/>
+        <Button variant="contained">Hello World</Button>
+        <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
       </header>
     </div>
   );
--- src/componets/myeditor/myeditor.js
+++ src/componets/myeditor/myeditor.js
@@ -5,12 +5,12 @@ class MyEditor extends React.Component {
   render() {
     return (
       <AvatarEditor
-        image="https://pixabay.com/get/g41375ea6528f896a321a7770f6acea79cfc92cfb058bf8a0668357acc7a02acad3b75a8b8f0f817e3cc1fae2afa8279c248bb9abe405833d8a4128e013161635_1280.jpg"
-        width={500}
-        height={500}
-        border={50}
+        image="https://pixabay.com/get/gad467af78146085466e8ace78e2783628f15ef106aa5c7caf671a4fea5370be033acca8bad7cc18777f757c4ca16935100c9719434d56a193bd6e749b0646570_1280.jpg"
+        width={250}
+        height={250}
+        border={25}
         color={[255, 255, 255, 0.6]} // RGBA
-        scale={1.2}
+        scale={1}
         rotate={0}
       />
     )
