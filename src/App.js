d2d1932101919d36831e42c05e99fa40a25944a9
yosbel-penate <yosbel.penate@gmail.com>
2021-10-02 00:59:09 +0300

    +++

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import './App.css';
 import MyEditor from './componets/myeditor/myeditor.js'
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Slider from '@mui/material/Slider';
-
-function App() {
-  return (
-    <div className="App">
+import React, { Component } from 'react'
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <MyEditor/>
-        <Button variant="contained">Hello World</Button>
-        <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
       </header>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
+
--- src/componets/myeditor/myeditor.js
+++ src/componets/myeditor/myeditor.js
@@ -1,18 +1,71 @@
 import React from 'react'
-import AvatarEditor from 'react-avatar-editor'
+import ReactAvatarEditor from 'react-avatar-editor'
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
 
 class MyEditor extends React.Component {
+
+  state = {
+    image: 'https://pixabay.com/get/gba04cc7484c97dfa7106cd85b0d03471e84d2f47af4a42a9b4c03a6002f2a86e02f7cc685c49d7d91e1b43df9686dbbe660b09a6bc49954f848b6f0305e7f2aa_1280.jpg',
+    allowZoomOut: false,
+    position: { x: 0.5, y: 0.5 },
+    scale: 1,
+    rotate: 0,
+    borderRadius: 0,
+    preview: null,
+    width: 200,
+    height: 200,
+  }
+
+  handleScale = e => {
+    const scale = parseFloat(e.target.value)
+    this.setState({ scale })
+  }
+  
+  handleRotate = e => {
+    const rotate = parseInt(e.target.value)
+    this.setState({ rotate })
+  }
+
+  handlePositionChange = position => {
+    this.setState({ position })
+  }
+
   render() {
     return (
-      <AvatarEditor
-        image="https://pixabay.com/get/gad467af78146085466e8ace78e2783628f15ef106aa5c7caf671a4fea5370be033acca8bad7cc18777f757c4ca16935100c9719434d56a193bd6e749b0646570_1280.jpg"
-        width={250}
-        height={250}
-        border={25}
-        color={[255, 255, 255, 0.6]} // RGBA
-        scale={1}
-        rotate={0}
-      />
+      <div>
+            <ReactAvatarEditor
+              scale={parseFloat(this.state.scale)}
+              width={this.state.width}
+              height={this.state.height}
+              position={this.state.position}
+              onPositionChange={this.handlePositionChange}
+              rotate={parseFloat(this.state.rotate)}
+              borderRadius={this.state.width / (100 / this.state.borderRadius)}
+              image={this.state.image}
+              className="editor-canvas"
+            />
+            <Box width={300}>
+                <input
+                  name="scale2"
+                  type="range"
+                  onChange={this.handleScale}
+                  min={this.state.allowZoomOut ? '0.1' : '1'}
+                  max="2"
+                  step="0.01"
+                  defaultValue="1"
+              />
+              <input
+                name="rotate"
+                type="range"
+                onChange={this.handleRotate}
+                min="0"
+                max="180"
+                step="1"
+                defaultValue="0"
+              />
+            </Box>
+          </div>
     )
   }
 }
